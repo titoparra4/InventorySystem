a18f1f866bd3a63eb9eeca1c1d5e736d2a8c3f3c
Tito <tito.parra4@@hotmail.com>
2020-09-21 12:47:02 +0200

    Users Management

---
+++ InventorySystem.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventorySystem.DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventorySystem.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var userList = _db.UserApplications.ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach(var user in userList)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+            }
+
+            return Json(new { data = userList });
+
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var user = _db.UserApplications.FirstOrDefault(u => u.Id == id);
+            if(user == null)
+            {
+                return Json(new { success = false, message = "User error" });
+            }
+
+            if(user.LockoutEnd!=null && user.LockoutEnd>DateTime.Now)
+            {
+                user.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                user.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Successful" });
+        }
+    }
+}
---
+++ InventorySystem.Web/wwwroot/js/user.js
@@ -0,0 +1,71 @@
+﻿var datatable;
+
+$(document).ready(function () {
+    loadDataTable();
+
+});
+
+function loadDataTable() {
+    datatable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/User/GetAll"
+        },
+        "columns": [
+            { "data": "userName", "width": "10%" },
+            { "data": "name", "width": "10%" },
+            { "data": "lastName", "width": "10%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "20%" },
+            { "data": "role", "width": "15%" },
+            {
+                "data": {
+                    id: "id", lockoutEnd: "lockoutEnd"
+                },
+                "render": function (data) {
+
+                    var today = new Date().getTime();
+                    var block = new Date(data.lockoutEnd).getTime();
+
+                    if (block > today) {
+                        return `
+                    <div class="text-center">
+                        <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:150px">
+                            <i class="fas fa-lock-open"></i> Unlock
+                        </a>
+                    </div>`;
+
+                    }
+                    else {
+                        return `
+                    <div class="text-center">
+                        <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:150px">
+                            <i class="fas fa-lock"></i> Lock
+                        </a>
+                    </div>`;
+                    }
+
+                }, "width": "30%"
+
+            }
+        ]
+    });
+}
+
+function LockUnlock(id) {
+            $.ajax({
+                type: "POST",
+                url: '/Admin/User/LockUnlock',
+                data: JSON.stringify(id),
+                contentType: "application/json",
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        datatable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+
+}
