7075bca32fb7818091859aec373a62cdef637457
Tito <tito.parra4@@hotmail.com>
2020-09-21 19:54:03 +0200

    Email sending configuration

--- InventorySystem.Utilities/EmailSender.cs
+++ InventorySystem.Utilities/EmailSender.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using System;
 using System.Collections.Generic;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,24 @@ namespace InventorySystem.Utilities
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            return Execute(subject, htmlMessage, email);
+        }
+
+        public Task Execute(string subject, string message, string email)
+        {
+            MailMessage mm = new MailMessage();
+            mm.To.Add(email);
+            mm.Subject = subject;
+            mm.Body = message;
+            mm.From = new MailAddress("test.titoparra4@outlook.com");
+            mm.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.sendgrid.net");
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = true;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new System.Net.NetworkCredential("apikey", "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");
+
+            return smtp.SendMailAsync(mm);
         }
     }
 }
